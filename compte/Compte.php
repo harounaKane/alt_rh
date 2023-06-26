0dc4776e6644a800c50d2d69e04fe161b0d5a0ee
harounaKane <harouna.kane@hotmail.fr>
2023-06-26 12:53:23 +0200

    typage des attributs

--- compte/Compte.php
+++ compte/Compte.php
@@ -1,13 +1,13 @@
 <?php
 
 class Compte{
-     public $numero;
-     public $prenom;
-     public $solde;
+     private int $numero;
+     private Personne $titulaire;
+     private float $solde;
 
-     public function __construct($numero, $prenom, $solde){
+     public function __construct(int $numero, Personne $titulaire, float $solde){
           $this->numero = $numero;
-          $this->prenom = $prenom;
+          $this->titulaire = $titulaire;
           $this->solde = $solde;
      }
 
@@ -24,4 +24,33 @@ class Compte{
           
      }
 
+     public function virerVers($montant, $compteDest){
+          $this->retirer($montant);
+          $compteDest->deposer($montant);
+     }
+
+     public function getNumero(){
+          return $this->numero;
+     }
+
+     public function getTitulaire() :Personne{
+          return $this->titulaire;
+     }
+
+     public function getSolde() :float{
+          return '$this->solde';
+     }
+
+     public function setTitulaire($titulaire){
+          $this->titulaire = $titulaire;
+     }
+
+     public function setSolde(float $montant){
+          $this->solde = $montant;
+     }
+
+     public function __toString()
+     {
+          return "numéero: " . $this->numero ." titulaire: " . $this->titulaire->prenom ." solde: ".  $this->solde;
+     }
 }
--- compte/index.php
+++ compte/index.php
@@ -1,17 +1,28 @@
 <?php
 
 include "Compte.php";
+include "Personne.php";
+$p1 = new Personne("Julie", "Dupond", 25, "Nantes");
+$c1 = new Compte(1001, new Personne("Jean", "Pierre", 35), 1500);
+$c2 = new Compte(1002, $p1, 250);
 
-$c1 = new Compte(1001, "Jeans", 1500);
-$c2 = new Compte(1002, "Julie", 250);
-
-echo $c1->solde;
+echo $c1;
 echo "<br>";
-echo $c2->solde;
+echo $c2; 
 
-echo "<br>";
+// echo $c1->getSolde();
+ echo "<br>";
+ echo $c2->getSolde();
 
+// echo "<br>";
+// $c1->virerVers(200, $c2);
 
-echo $c1->solde;
-echo "<br>";
-echo $c2->solde;
+// echo $c1->getSolde();
+// echo "<br>";
+// echo $c2->getSolde();
+
+// echo $c1->getTitulaire();
+// $c1->setSolde("truc");
+
+// echo "<br>";
+// echo $c1->getSolde();
